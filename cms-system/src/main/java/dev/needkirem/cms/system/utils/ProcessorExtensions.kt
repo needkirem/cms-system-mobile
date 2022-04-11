e87dae8bd488d83895d987d4f7e3b394ef3879c2
Kirill <needkirem@gmail.com>
2022-04-11 23:22:13 +0300

    Add new processor extensions

--- cms-system/src/main/java/dev/needkirem/cms/system/utils/ProcessorExtensions.kt
+++ cms-system/src/main/java/dev/needkirem/cms/system/utils/ProcessorExtensions.kt
@@ -2,10 +2,9 @@ package dev.needkirem.cms.system.utils
 
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.asTypeName
-import javax.lang.model.element.Element
-import javax.lang.model.element.ElementKind
-import javax.lang.model.element.PackageElement
-import javax.lang.model.element.TypeElement
+import javax.annotation.processing.ProcessingEnvironment
+import javax.lang.model.element.*
+import javax.lang.model.type.TypeMirror
 import kotlin.reflect.KClass
 
 val DECLARED_ELEMENT_KINDS = setOf(
@@ -15,23 +14,11 @@ val DECLARED_ELEMENT_KINDS = setOf(
     ElementKind.ANNOTATION_TYPE
 )
 
-fun TypeElement.equalTo(type: KClass<*>): Boolean {
-    return this.qualifiedName.contentEquals(type.java.name)
-}
-
-fun Collection<TypeElement>.containsType(type: KClass<*>): Boolean {
-    return this.any { element -> element.equalTo(type) }
-}
-
 fun Element.asTypeElement(): TypeElement {
     if (!DECLARED_ELEMENT_KINDS.contains(kind)) {
         error("Cannot cast element $this to TypeElement, because in fact it is of kind $kind!")
     }
     return this as TypeElement
-}
-
-fun TypeElement.getClassName(): String {
-    return simpleName.toString()
 }
 
 fun Element.getPackage(): PackageElement {
@@ -42,6 +29,60 @@ fun Element.getPackage(): PackageElement {
     return element as PackageElement
 }
 
+fun Element.getClassNameString(): String {
+    return simpleName.toString()
+}
+
+fun Element.getTypeName(): TypeName {
+    return asTypeElement().getTypeName()
+}
+
+fun TypeElement.equalTo(type: KClass<*>): Boolean {
+    return this.qualifiedName.contentEquals(type.java.name)
+}
+
+fun Collection<TypeElement>.containsType(type: KClass<*>): Boolean {
+    return this.any { element -> element.equalTo(type) }
+}
+
+fun TypeElement.getClassName(): String {
+    return simpleName.toString()
+}
+
 fun TypeElement.getTypeName(): TypeName {
     return asType().asTypeName()
+}
+
+fun TypeElement.getAnnotationMirror(annotationClass: Class<*>): AnnotationMirror? {
+    for (annotationMirror in annotationMirrors) {
+        if (annotationMirror.annotationType.toString() == annotationClass.name) {
+            return annotationMirror
+        }
+    }
+    return null
+}
+
+fun AnnotationMirror.getAnnotationValueAsString(key: String): String {
+    return getAnnotationValue(key)?.value as String
+}
+
+fun AnnotationMirror.getAnnotationValue(key: String): AnnotationValue? {
+    for (elementValue in elementValues.entries) {
+        if (elementValue.key.simpleName.toString() == key) {
+            return elementValue.value
+        }
+    }
+    return null
+}
+
+fun AnnotationMirror.getAnnotationValueElement(
+    environment: ProcessingEnvironment,
+    key: String,
+): Element? {
+    val value = getAnnotationValue(key)?.value
+    return if (value != null) {
+        environment.typeUtils.asElement(value as TypeMirror)
+    } else {
+        null
+    }
 }
