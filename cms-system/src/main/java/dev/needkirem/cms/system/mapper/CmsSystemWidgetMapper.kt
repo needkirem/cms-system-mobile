1f21e1109f8212742d23c705e796fb0b294ff5fb
Kirill <needkirem@gmail.com>
2022-04-16 15:43:39 +0300

    Fix mapper contract

--- cms-system/src/main/java/dev/needkirem/cms/system/mapper/CmsSystemWidgetMapper.kt
+++ cms-system/src/main/java/dev/needkirem/cms/system/mapper/CmsSystemWidgetMapper.kt
@@ -2,7 +2,7 @@ package dev.needkirem.cms.system.mapper
 
 import dev.needkirem.cms.system.model.WidgetModel
 import dev.needkirem.cms.system.utils.getString
-import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.jsonObject
 
 class CmsSystemWidgetMapper(
@@ -10,7 +10,7 @@ class CmsSystemWidgetMapper(
     private val modelWidgetMapperProvider: WidgetMapperProvider,
 ) {
 
-    fun map(jsonWidgets: List<JsonObject>): List<WidgetModel> {
+    fun map(jsonWidgets: JsonArray): List<WidgetModel> {
         return jsonWidgets.mapNotNull { jsonElement ->
             val widgetType = jsonElement.jsonObject.getString("widgetType").orEmpty()
             val jsonMapper = jsonWidgetMapperProvider.get(widgetType)
